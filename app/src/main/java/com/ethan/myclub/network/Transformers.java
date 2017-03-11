85df9a08a1d0287ee2c5fdf50e702c54a245f128
Ethan <450821350@qq.com>
2017-03-11 16:18:08 +0800

    更新API的错误处理部分

--- app/src/main/java/com/ethan/myclub/network/Transformers.java
+++ app/src/main/java/com/ethan/myclub/network/Transformers.java
@@ -1,9 +1,14 @@
 package com.ethan.myclub.network;
 
+import android.util.Log;
+
+import com.ethan.myclub.models.network.Error;
 import com.ethan.myclub.network.exceptions.ApiException;
 import com.ethan.myclub.network.exceptions.ExceptionEngine;
 import com.ethan.myclub.network.exceptions.ServerException;
 import com.ethan.myclub.models.network.Response;
+
+import java.util.List;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
@@ -20,13 +25,22 @@ public class Transformers {
 
     public static class sTransformer<T> implements ObservableTransformer<Response<T>, T> {
 
+        private static final String TAG = "Transformers";
+
         @Override
         public ObservableSource<T> apply(Observable<Response<T>> upstream) {
             return upstream.map(new Function<Response<T>, T>() {
                 @Override
                 public T apply(Response<T> tResponse) throws Exception {
-                    if (tResponse.retCode != 0)
-                        throw new ServerException(tResponse.retCode, tResponse.detail);
+                    if (tResponse.code != 0)
+                    {
+                        List<Error> errors = tResponse.errors;
+                        if(errors != null && errors.size() != 0)
+                            throw new ServerException(tResponse.code, errors.get(0).message);
+                        else
+                            throw new ServerException(tResponse.code, tResponse.message);
+                    }
+
                     return tResponse.data;
                 }
             }).onErrorResumeNext(new HttpResponseHandler<T>());
