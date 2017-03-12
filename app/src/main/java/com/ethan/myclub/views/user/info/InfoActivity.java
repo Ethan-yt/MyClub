d9e85065d19587adba95fb50d8986d80475f87a3
Ethan <450821350@qq.com>
2017-03-12 14:57:35 +0800

    增加默认头像 增加圆形头像view

---
+++ app/src/main/java/com/ethan/myclub/views/user/info/InfoActivity.java
@@ -0,0 +1,15 @@
+package com.ethan.myclub.views.user.info;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ethan.myclub.R;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
