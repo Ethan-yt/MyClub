e7a338366c1dba81ebad9603317a95373a161ee3
Ethan <450821350@qq.com>
2017-04-16 05:57:18 +0800

    更新关于页面中使用到的框架

--- app/src/main/java/com/ethan/myclub/settings/SettingsFragment.java
+++ app/src/main/java/com/ethan/myclub/settings/SettingsFragment.java
@@ -23,19 +23,42 @@ import com.tencent.bugly.beta.Beta;
 import io.reactivex.functions.Consumer;
 
 public class SettingsFragment extends PreferenceFragment {
+    private static final String[] LIBRARIES = {
+            "ahbottomnavigation",
+            "gson",
+            "glide",
+            "Jsoup",
+            "OkHttp",
+            "Retrofit",
+            "rxjava",
+            "rxandroid",
+
+    };
+    private static final String[] EXCLUDED_LABRARIES = {
+            "AndroidIconics",
+            "fastadapter",
+            "appcompat_v7",
+            "design",
+            "recyclerview_v7",
+            "support_v4"
+    };
+
     @Override
     public void onCreatePreferences(Bundle bundle, String s) {
         // Load the Preferences from the XML file
         addPreferencesFromResource(R.xml.app_preferences);
         findPreference("about").setOnPreferenceClickListener(
                 new Preference.OnPreferenceClickListener() {
-                    @Override public boolean onPreferenceClick(Preference preference) {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
                         //config About Library
                         //new Libs.Builder().withActivityTitle("About").withFields(R.string.class.getFields()).start(getActivity());
                         new LibsBuilder()
                                 //provide a style (optional) (LIGHT, DARK, LIGHT_DARK_TOOLBAR)
                                 .withActivityStyle(Libs.ActivityStyle.LIGHT)
                                 .withActivityTitle("关于")
+                                .withLibraries(LIBRARIES)
+                                .withExcludedLibraries(EXCLUDED_LABRARIES)
                                 //start the activity
                                 .start(getActivity());
                         return false;
@@ -43,14 +66,16 @@ public class SettingsFragment extends PreferenceFragment {
                 });
         findPreference("check_update").setOnPreferenceClickListener(
                 new Preference.OnPreferenceClickListener() {
-                    @Override public boolean onPreferenceClick(Preference preference) {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
                         Beta.checkUpgrade();
                         return false;
                     }
                 });
         findPreference("logout").setOnPreferenceClickListener(
                 new Preference.OnPreferenceClickListener() {
-                    @Override public boolean onPreferenceClick(Preference preference) {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
                         new AlertDialog.Builder(SettingsFragment.this.getActivity())
                                 .setMessage("确定要退出吗")
                                 .setPositiveButton("确定", new DialogInterface.OnClickListener() {
